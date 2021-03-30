dab1df781f2caf7cfa54705b640c5dcf6242ff13
hoanglongdev533@gmail.com <hoanglongdev533@gmail.com>
2021-03-30 13:53:07 +0700

    Add project files.

---
+++ NguyenHoangLong_5951071052/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NguyenHoangLong_5951071052
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            float fCPU = pCpu.NextValue();
+            float fRAM = pRam.NextValue();
+
+            metroProgressBarCPU.Value = (int)fCPU;
+            metroProgressBarRAM.Value = (int)fRAM;
+
+            lblCPU.Text = string.Format("{0:0.00}%", fCPU);
+            lblRAM.Text = string.Format("{0:0.00}%", fRAM);
+        }
+    }
+}
